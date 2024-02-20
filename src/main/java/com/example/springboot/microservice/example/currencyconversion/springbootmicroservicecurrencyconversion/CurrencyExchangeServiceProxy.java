98665f4cadf114a60b98c83c395a4a443b83cf35
Rihab <elifarihab@gmail.com>
2024-02-20 18:12:33 +0100

    version 1

---
+++ src/main/java/com/example/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,13 @@
+package com.example.springboot.microservice.example.currencyconversion.springbootmicroservicecurrencyconversion;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name="forex-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversionBean retrieveExchangeValue
+            (@PathVariable("from") String from, @PathVariable("to") String to);
+}
